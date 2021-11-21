901a20f244632d8f2330dd9a45942027a93ecb3b
zhenyuefu <zhenyue.fu@etu.upmc.fr>
2021-11-21 02:06:05 +0100

    优化文件读取

--- src/main/java/indi/zhenyue/networkanalyser/util/FileUtility.java
+++ src/main/java/indi/zhenyue/networkanalyser/util/FileUtility.java
@@ -1,25 +1,20 @@
 package indi.zhenyue.networkanalyser.util;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FileUtility {
 
     public static String readFile(File f) {
         String readString = "";
-
-        char[] readIn = new char[(Long.valueOf(f.length())).intValue()];
-
-        try (BufferedReader in = new BufferedReader(new FileReader(f))) {
-
+        byte[] readIn = new byte[(Long.valueOf(f.length())).intValue()];
+        try (FileInputStream in = new FileInputStream(f)) {
             in.read(readIn);
             readString = new String(readIn);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return readString;
     }
 }
